9b738fe6736f8b6774a552a59903244f88f23ba5
Reut Riklin <57776203+reutriklin@users.noreply.github.com>
2022-03-16 01:52:16 +0200

    Delete ConnectedVessels5_4.py

--- ConnectedVessels5_4.py
+++
@@ -1,9 +0,0 @@
-def interleave(*parameters):
-  listReturn=[]
-  for i,element in enumerate(parameters):
-    for param in parameters:
-      listReturn+=[param[i]]
-  return listReturn
-
-
-#interleave('abc', [1, 2, 3], ('!', '@', '#'))
